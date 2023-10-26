5df0a8b78315017cc752daf0fb1c12ddd0205223
Mokhtar Yosofzay <mokhtar.yosofzay@studmail.w-hs.de>
2023-10-26 23:51:17 +0200

    Erweitere Backend mit Nutzer-Anmeldung
    - Teile die Anfragen in einzelne Routen auf
    - Erstelle Controller und Modelle für Nutzer
    - Passe Registrierung an
    - Implementiere Anmelden

---
+++ backend/middleware/verifySignUp.js
@@ -0,0 +1,49 @@
+const db = require("../models");
+const ROLES = db.ROLES;
+const User = db.user;
+
+/**
+ * Prüfe, ob eine E-Mail Adresse bereits in Verwendung ist
+ */
+checkDuplicateEmail = (req, res, next) => {
+  // Email
+  User.findOne({
+      where: {
+      email: req.body.email
+      }
+  }).then(user => {
+      if (user) {
+      res.status(400).send({
+        message: "E-Mail Adresse bereits vergeben!"
+      });
+      return;
+      }
+
+      next();
+  });
+};
+
+/**
+ * Prüfe, ob die Rolle existiert
+ */
+checkRolesExisted = (req, res, next) => {
+  if (req.body.roles) {
+    for (let i = 0; i < req.body.roles.length; i++) {
+      if (!ROLES.includes(req.body.roles[i])) {
+        res.status(400).send({
+          message: "Die Rolle existiert nicht = " + req.body.roles[i]
+        });
+        return;
+      }
+    }
+  }
+  
+  next();
+};
+
+const verifySignUp = {
+  checkDuplicateEmail: checkDuplicateEmail,
+  checkRolesExisted: checkRolesExisted
+};
+
+module.exports = verifySignUp;
