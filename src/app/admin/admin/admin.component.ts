8bc6da9faf12fcd5441dd90a88f0c7d890d894be
Arnauld-Mba <Arnauld.Kuitche@studmail.w-hs.de>
2023-10-30 18:47:38 +0100

    Verbindung mit DB

--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -11,18 +11,34 @@ import { Nutzer } from 'src/models/nutzer.model';
 
 export class AdminComponent implements OnInit {
 
-  users: any [] = ["test, test"];
-  
+  users: any[] = [];
+  hintergrund ="";
 
   constructor(private nutzerService: NutzerService) {}
 
    ngOnInit(): void {
      console.log("Hallo hier");
      this.nutzerService.getUsers().subscribe((data) => {
-       this.users.push(data) ;
+       this.users = data;
      })
-     console.log(this.users)
    }
+
+   bearbeiten(user: string) {
+
+      console.log(user);
+  }
+
+  changeBackgroundColor(user: string): string {
+
+    for(let i = 0; i < this.users.length; i++){
+      if(this.users[i] == user && i % 2 == 0){
+        this.hintergrund = "white";
+      }else{
+        this.hintergrund = "#00528d49";
+      }
+    }
+    return this.hintergrund;
+  }
 
   /**ngOnInit(): void {
     console.log("Hallo hier");
