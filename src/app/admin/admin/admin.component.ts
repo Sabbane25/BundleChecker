66b8eba6a34351617edb3d4ef924c2531c30c463
Yahya <yahyasabbane@gmail.com>
2023-11-13 23:13:55 +0100

    Problem getNutzer behoben

--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -15,28 +15,41 @@ export class AdminComponent implements OnInit {
 
   @ViewChild('emailInput', { static: false }) emailInput: ElementRef;
 
-  users: Nutzer[] = [{id: 2, email: "test", password: "password"}];
-
+  users: any[] = [];
+  loading: boolean = true;
+  error: string = '';
   constructor(private nutzerService: NutzerService) {}
   
 
   ngOnInit(): void {
       this.nutzerService.getUsers().subscribe((data) => {
       this.users =  data;
-  })
-}
+    })
+  }
   
 
   onClick(event: KeyboardEvent) {
     if(event.key === 'Enter') {
       this.searchUser();
     }
+
+    this.nutzerService.getUsers().subscribe(
+      (data) => {
+        console.log('Daten vom Backend erhalten:', data);
+        this.users = data;
+        this.loading = false;
+      },
+      (error) => {
+        this.error = 'Fehler beim Laden der Nutzerdaten.';
+        this.loading = false;
+      }
+    );
   }
 
   searchUser() {
     const suchanfrage = this.emailInput.nativeElement.value;
     this.nutzerService.suchen(suchanfrage);
-}
+  }
    
   userLoeschen(user_id: number): void {
       console.log("component.ts");
