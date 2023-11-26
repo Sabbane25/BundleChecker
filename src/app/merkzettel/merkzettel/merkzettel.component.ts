f3b717b0708ce2300d36013e8a480b7b18c3a889
Mokhtar Yosofzay <mokhtar.yosofzay@studmail.w-hs.de>
2023-11-27 00:59:07 +0100

    Merkzettel Ausgabe
    Gebe Merkzettel im Frontend als Liste aus
    Gebe Produkte zu einem Merkzettel aus
    Ermögliche das Entfernen von Produkten aus einem Merkzettel

--- src/app/merkzettel/merkzettel/merkzettel.component.ts
+++ src/app/merkzettel/merkzettel/merkzettel.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Artikel } from 'src/models/artikel.model';
+import {HttpClient} from "@angular/common/http";
+import {MerkzettelService} from "../../../services/merkzettel.service";
+import {TokenStorageService} from "../../../services/token-storage.service";
 
 
 @Component({
@@ -9,13 +12,67 @@ import { Artikel } from 'src/models/artikel.model';
 })
 export class MerkzettelComponent {
   merkzettelArtikel: Artikel[] = []; // Hier werden die Artikel im Merkzettel gespeichert
+  bundles: any = [];
+  selectedBundleId: number;
+  selectedBundleLabel: string;
+
+  constructor(private http: HttpClient, private merkzettelService: MerkzettelService, private tokenStorageService: TokenStorageService) {}
+
+  ngOnInit(): void {
+    this.holeMerkzettel();
+  }
 
   // Methode, um Artikel aus dem Merkzettel zu entfernen
-  removeItemFromMerkzettel(artikel: Artikel) {
+  removeItemFromMerkzettel(selectedBundleId: number, artikel: Artikel) {
+    this.merkzettelService.removeItemFromMerkzettel(selectedBundleId, artikel);
+
     const index = this.merkzettelArtikel.indexOf(artikel);
     if (index !== -1) {
       this.merkzettelArtikel.splice(index, 1);
     }
   }
 
+  /**
+   * Hole eine Liste aller Merkzettel
+   */
+  async holeMerkzettel() {
+    // @ts-ignore
+    const merkzettel: []|undefined = await this.merkzettelService.holeMerkzettel();
+
+    if (merkzettel !== undefined) {
+      for (let id in merkzettel) {
+        const bundle: {id: number, label: string} = merkzettel[id];
+        const label = bundle.label ? bundle.label : 'Bundle ' + bundle.id;
+
+        // @ts-ignore
+        const price: {price: number}|undefined = await this.merkzettelService.berechnePreis(bundle.id);
+
+        this.bundles[id] = {
+          id_list: id,
+          id: bundle.id,
+          label: label,
+          preis: price ? price?.price : '-',
+          produkte: await this.merkzettelService.holeProdukte(bundle.id)
+        };
+      }
+
+      console.log(this.bundles)
+    }
+  }
+
+  /**
+   * Zeige die Produkte eines Merkzettels an
+   *
+   * @param id_list
+   */
+  zeigeProdukte(id_list: number){
+    this.merkzettelArtikel = [];
+    this.selectedBundleId = this.bundles[id_list].id;
+    this.selectedBundleLabel = this.bundles[id_list].label;
+
+    for (let produkt in this.bundles[id_list].produkte) {
+      const artikel: Artikel = this.bundles[id_list].produkte[produkt];
+      this.merkzettelArtikel.push(artikel);
+    }
+  }
 }
