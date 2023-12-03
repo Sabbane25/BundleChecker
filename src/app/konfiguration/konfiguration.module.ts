4c9fdf421679a5cc2def4cacd43709ebe2e5e893
Mokhtar Yosofzay <mokhtar.yosofzay@studmail.w-hs.de>
2023-12-03 16:19:47 +0100

    Merkzettel löschen
    Lösche einen ganzen Merkzettel

--- src/app/konfiguration/konfiguration.module.ts
+++ src/app/konfiguration/konfiguration.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UebersichtComponent } from './uebersicht/uebersicht.component'; // Stellen Sie sicher, dass der Pfad korrekt ist.
 import { KonfigurationComponent } from './konfiguration/konfiguration.component';
-import { RouterModule, Routes } from '@angular/router';
+import {RouterLink, RouterModule, Routes} from '@angular/router';
 import { ListKomponenteComponent } from './list-komponente/list-komponente.component';
 import { DropDownMenuComponent } from '../drop-down-menu/drop-down-menu.component';
 import { FilterComponent } from '../filter/filter.component';
@@ -16,7 +16,7 @@ import { FilterComponent } from '../filter/filter.component';
     DropDownMenuComponent,
     FilterComponent
   ], // Hier die Komponenten deklarieren.
-  imports: [CommonModule],
+    imports: [CommonModule, RouterLink],
   exports: [KonfigurationComponent], // Wenn die KonfigurationComponent exportiert werden muss.
 
 })
